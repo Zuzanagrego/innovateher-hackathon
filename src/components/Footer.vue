7fe87918f8b3de143500ed0f1d8a59e12bfd7145
Zuzana <zuzanagrego@gmail.com>
2024-03-02 17:15:13 -0500

    Add button labels

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,13 +10,27 @@ defineProps({
 <template>
   <footer >
 <div class="footer">
+  <div class="column">
     <i class="fas fa-home"></i>
+    <a>Home</a>
+  </div>
+  <div class="column">
     <i class="fas fa-search"></i>
+    <a>Search</a>
+  </div>
+  <div class="column">
     <i class="fas fa-briefcase"></i>
+    <a>Jobs</a>
+  </div>
+  <div class="column">
     <i class="fas fa-file"></i>
+    <a>Learn</a>
+  </div>
+  <div class="column">
     <i class="fas fa-user"></i>
+    <a>Profile</a>
+  </div>
 </div>
-    
   </footer>
 </template>
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,15 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Footer from "../components/Footer.vue"
+
+export default {
+  name: 'HomeView',
+  components: {
+    Footer
+  }
+}
+</script>
