2d3b1f5762a9fbc8b7721c6752bdabea4335575c
Betaser <tylerharrisonhsieh@gmail.com>
2024-03-02 17:10:33 -0500

    Starting the certificates page, just rendering directly to home page for now

---
+++ src/components/Certificate.vue
@@ -0,0 +1,25 @@
+<script setup>
+const skills = ["Javascript", "Vue"];
+</script>
+
+<template>
+    <div>
+        <h1>List the certificates</h1>
+    </div>
+
+    <ul style="list-style: none;">
+        <template v-for="skill in skills">
+            <li class="certificate-button">
+                <input type="b-button"
+                    :value=skill>
+            </li>
+        </template>
+    </ul>
+</template>
+
+<style>
+.certificate-button {
+    display: inline-block;    
+    padding: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     Hello
+    <Certificate />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Footer from "../components/Footer.vue"
+import Certificate from "../components/Certificate.vue";
 
 export default {
   name: 'HomeView',
   components: {
+    Certificate,
     Footer
   }
 }
