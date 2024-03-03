7eb5059085ca315bebd61ac8e385f928ed6e0e37
Zuzana <zuzanagrego@gmail.com>
2024-03-03 01:23:05 -0500

    create profile content

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,4 +3,29 @@
 
 <template>
   <h1>Profile</h1>
+  <div class="profile-wrapper">
+    <div class="profile-main profile">
+      <div class="flex">
+        <img src="../assets/soft-girl-aesthetic.jpg"/>
+      <h1>Innovate Her</h1>
+      </div>
+      <h2>
+        Hello everyone, I am super excited to learn more about coding! I started with HTML and CSS and I'm hoping to ear the IBM certificate for Full-Stack developers
+      </h2>
+    </div>
+    <div class="profile-certificates profile">
+      <h2>
+        Certificate of attending InnovateHER Hackathon 2024
+      </h2>
+      <h2>
+        Certificate of attending InnovateHER Hackathon 2024
+      </h2>
+      <h2>
+        Certificate of attending InnovateHER Hackathon 2024
+      </h2>
+      <h2>
+        Certificate of attending InnovateHER Hackathon 2024
+      </h2>
+    </div>
+  </div>
 </template>
